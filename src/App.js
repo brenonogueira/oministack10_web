fe7606227bb08bba341efcbb3b55fa2cd275a8a2
Breno Nogueira Araújo <breno.nog.araujo@hotmail.com>
2021-06-02 00:39:56 -0400

    ligando api com o front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 
 function App() {
+
+  const [devs, setDevs] = useState([]); //armazenar num vetor o vetor que vem da api com os dados de devs
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data)
+    }
+
+    loadDevs(); //chamando função assim que o useEffect for executando o get
+  }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    console.log(response.data)
+
+    setDevs([...devs, response.data]); //adiciona todos os devs já existente, depois adiciona o novo
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+
+        </ul>
+      </main>
+
     </div>
   );
 }
